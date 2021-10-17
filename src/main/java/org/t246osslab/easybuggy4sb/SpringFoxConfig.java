54dc051bdc70cee334cb80bd8919197473bdc4f0
Eli Finkel <eyfinkel@gmail.com>
2021-10-17 14:31:07 +0300

    - updated springboot version to latest (added missing deps and fixed some imports) - updated swagger after adding ignore rules for superfluous types - updated output file name

--- src/main/java/org/t246osslab/easybuggy4sb/SpringFoxConfig.java
+++ src/main/java/org/t246osslab/easybuggy4sb/SpringFoxConfig.java
@@ -1,9 +1,13 @@
 package org.t246osslab.easybuggy4sb;
 
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ModelAndView;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
+import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.BasicAuth;
 import springfox.documentation.service.SecurityReference;
@@ -16,6 +20,7 @@ import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 import static org.t246osslab.easybuggy4sb.WebSecurity.AUTH_PATH_SWAGGER_REGEX;
 
@@ -24,14 +29,17 @@ import static org.t246osslab.easybuggy4sb.WebSecurity.AUTH_PATH_SWAGGER_REGEX;
 public class SpringFoxConfig {
 
     @Bean
-    public Docket api() {
+    public Docket api(TypeResolver typeResolver) {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(Arrays.asList(oauth()))
-                .securityContexts(Arrays.asList(securityContext()));
+                .securityContexts(Arrays.asList(securityContext()))
+                .ignoredParameterTypes(Locale.class)
+                .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(ModelAndView.class),
+                        typeResolver.resolve(WildcardType.class)));
     }
 
     @Bean
