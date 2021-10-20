bd55f27072b794a54e2277755155ca989150ef43
Eli Finkel <eyfinkel@gmail.com>
2021-10-20 15:09:47 +0300

    CxController: added some apis and updated swagger

--- src/main/java/org/t246osslab/easybuggy4sb/controller/CxController.java
+++ src/main/java/org/t246osslab/easybuggy4sb/controller/CxController.java
@@ -1,41 +1,62 @@
 package org.t246osslab.easybuggy4sb.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 @RestController
 public class CxController {
 
-	@GetMapping("v2/authed/getTime") // require auth
-	public String getTime() {
-		return new Date().toString();
-	}
+    @GetMapping("v2/authed/getTime") // require auth
+    public String getTime() {
+        return new Date().toString();
+    }
 
-	// curl localhost:8080/legacy/runCommand/whoami
-	@GetMapping("legacy/runCommand/{cmd}")
-	public String runCommand(@PathVariable String cmd) throws IOException {
-		byte[] buf = new byte[1024];
-		int len = Runtime.getRuntime().exec(cmd).getInputStream().read(buf);
-		return new String(buf, 0, len);
-	}
+    @GetMapping("v2/authed/getUser") // require auth
+    public String getUser() {
+        return "user is: " + System.getProperty("user.name");
+    }
 
-	@GetMapping("legacy/internal")
-	public String internal() {
-		return "this is an internal api";
-	}
+    @GetMapping("v2/authed/getIP") // require auth
+    public String getIP() throws UnknownHostException {
+        return Inet4Address.getLocalHost().getHostAddress();
+    }
 
-	@GetMapping("legacy/op1")
-	public String op1() {
-		return "op1 api";
-	}
+    @ApiIgnore // don't want this in openapi file
+    @GetMapping("v2/authed/multiply") // require auth
+    public int multiply(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b) {
+        return a * b;
+    }
 
-	@GetMapping("legacy/op2")
-	public String op2() {
-		return "op2 api";
-	}
+    // curl localhost:8080/legacy/runCommand/whoami
+    @PostMapping("legacy/runCommand/{cmd}")
+    public String runCommand(@PathVariable String cmd) throws IOException {
+        byte[] buf = new byte[1024];
+        int len = Runtime.getRuntime().exec(cmd).getInputStream().read(buf);
+        return new String(buf, 0, len);
+    }
+
+    @GetMapping("legacy/add")
+    public int add(@RequestParam(name = "a") int a, @RequestParam(name = "b") int b) {
+        return a + b;
+    }
+
+    @GetMapping("internal")
+    public String internal() {
+        return "this is an internal api";
+    }
+
+    @GetMapping("internal/op1")
+    public String op1() {
+        return "op1 api";
+    }
+
+    @PostMapping("internal/op2")
+    public String op2() {
+        return "op2 api";
+    }
 }
